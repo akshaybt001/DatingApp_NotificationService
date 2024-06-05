355f30528c13ebd81ae33d9643d7dfc36fea822b
Akshay Babu <akshaybabut1@gmail.com>
2024-06-05 20:22:37 +0530

    small changes

--- internal/helper/otp/otp.go
+++ internal/helper/otp/otp.go
@@ -17,7 +17,7 @@ var redisClient *redis.Client
 func init() {
 	fmt.Println("hii from init redis")
 	redisClient = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "redis-service:6379",
 		Password: "",
 		DB:       0,
 	})
